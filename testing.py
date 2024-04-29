6349a072d5c307110330907d063ca6efacf309a2
Patrick Yuan <py95@cornell.edu>
2024-04-28 20:51:52 -0400

    implement separate weights for each smolgen head

--- testing.py
+++ testing.py
@@ -2,61 +2,38 @@ import torch
 import torch.nn as nn
 
 
-class RMSNorm(nn.Module):
-    def __init__(self, depth, eps=1e-7):
-        super().__init__()
-        self.norm = nn.LayerNorm(depth)
+@torch.jit.script
+def batch_mul_ref(W, B, X):
+    # @ operator broadcasts and uses too much memory
+    # W shape: num_heads, D_out, D_in
+    # B shape: num_heads, D_out
+    # X shape: batch size, num_heads, D_in
+    # returns: W @ X + B of shape batch size, num_heads, D_out
 
-    def forward(self, t):
-        return self.norm(t)
+    return (W @ X.unsqueeze(-1)).squeeze(-1) + B
 
 
 @torch.jit.script
-def scaled_dot_product_attention(keys, queries, values, attention_mask, num_heads):
-    # assumes keys and values are of shape N, S, D, and queries N, L, D.
-    N, S, D = keys.shape
-    _, L, _ = queries.shape
-    keys = keys.reshape(N, S, num_heads, D // num_heads).transpose(-2, -3)
-    queries = queries.reshape(N, L, num_heads, D // num_heads).transpose(-2, -3)
-    values = values.reshape(N, S, num_heads, D // num_heads).transpose(-2, -3)
-    scale = 1 / torch.sqrt(keys.shape[-1])
-    attn_weights = queries @ keys.transpose(-1, -2) * scale + attention_mask
-    attn_weights = attn_weights.softmax(-1)
-    output = attn_weights @ values  # N, ..., num_heads, L, D // num_heads
-    output = output.transpose(-2, -3)  # N, ..., L, num_heads, D // num_heads
-    output = output.flatten(-2, -1)
-    return output, attn_weights
+def batch_mul(W, B, X):
+    # @ operator broadcasts and uses too much memory
+    # W shape: num_heads, D_out, D_in
+    # B shape: num_heads, D_out
+    # X shape: batch size, num_heads, D_in
+    # returns: W @ X + B of shape batch size, num_heads, D_out
+    x = torch.einsum("hij, bhj->bhi", W, X)
+    x += B
+    return x
 
 
-class Encoder(nn.Module):
-    def __init__(self, depth_in, dff, depth_out, activation, eps=1e-5):
-        super().__init__()
-        self.d_in = depth_in
-        self.d_out = depth_out
-        self.kqv = nn.Linear(depth_in, depth_in * 3, bias=False)
-        self.ffn = nn.Sequential(
-            nn.Linear(depth_in, dff, bias=True),
-            activation,
-            nn.Linear(dff, depth_out, bias=True),
-        )
-        self.norm = RMSNorm(depth_out, eps=eps)
+from time import time
 
-    def forward(self, x, attn_mask, num_heads):
-        """
-        shape of x: N, L, D_in
-        shape of attn_mask: N, num_heads, L, L
-        returns: tensor of shape N, L, D_out
-        """
-        N, L, _ = x.shape
-        k, q, v = self.kqv(x).split(self.d_in, dim=-1)
-        output, _ = scaled_dot_product_attention(k, q, v, attn_mask, num_heads)
-        output = self.norm(self.ffn(output))
-        x = x.reshape(N, L, -1, self.d_out).mean(-2)
-        return x + output
+W = torch.randn(16, 4096, 64) * 100
+B = torch.randn(16, 4096) * 100
+X = torch.randn(2048, 16, 64) * 100
 
-
-encoder = Encoder(256, 128, 128, nn.ReLU())
-num_heads = torch.tensor(8)
-x = torch.rand(4, 4, 256)
-attn_mask = torch.eye(4) + torch.zeros(1, 1, 1, 1)
-print(encoder(x, attn_mask, num_heads).shape)
+start = time()
+for _ in range(50):
+    print(_)
+    cur = batch_mul(W, B, X)
+    # print((W @ X.unsqueeze(-1)).shape)
+print(time() - start)
